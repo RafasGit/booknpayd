428a0a9b416db88a600e89b020a26c92c39a748a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-22 22:03:37 +0300

    INITIAL

---
+++ app/_components/sidebar-routes.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { BarChart, Compass, Layout, List } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { SidebarItem } from './sidebar-item'
+
+const guestRoutes = [
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/',
+  },
+  {
+    icon: Layout,
+    label: 'Courses for you',
+    href: '/search',
+  },
+]
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: 'Courses',
+    href: '/teacher/courses',
+  },
+  {
+    icon: BarChart,
+    label: 'Analytics',
+    href: '/teacher/analytics',
+  },
+]
+
+export const SidebarRoutes = () => {
+    const pathname = usePathname()
+  
+    const isTeacherPage = pathname?.startsWith('/teacher')
+  
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes
+    return (
+      <div className="flex w-full flex-col">
+        {routes.map((route) => (
+          <SidebarItem key={route.href} icon={route.icon} label={route.label} href={route.href} />
+        ))}
+      </div>
+    )
+}
